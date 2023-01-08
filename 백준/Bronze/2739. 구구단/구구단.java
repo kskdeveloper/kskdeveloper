9958c5ff6c0905b91ac1ea4ade51ccc34b600efc
KwonSuKyung <116548534+kskdeveloper@users.noreply.github.com>
2023-01-08 22:16:51 +0900

    [Bronze V] Title: 구구단, Time: 244 ms, Memory: 18552 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {  
+  public static void main(String args[]) { 
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    
+    for(int i = 1; i<10; i++) {
+    	int result = N*i;
+    	System.out.println(N +" * " + i + " = " + result);
+    }
+  } 
+}
