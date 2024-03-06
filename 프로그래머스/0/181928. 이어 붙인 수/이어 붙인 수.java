84848d1ba821159d552567986a5c840e14b45b97
KwonSuKyung <116548534+kskdeveloper@users.noreply.github.com>
2024-03-06 19:42:54 +0900

    [level 0] Title: 이어 붙인 수, Time: 1.07 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String num1 = "";
+        String num2 = "";
+        for(int i =0; i<num_list.length; i++){
+            if(num_list[i]%2==0){
+                num1 += Integer.toString(num_list[i]);
+            } else {
+                num2 += Integer.toString(num_list[i]);
+            }
+        }
+        int answer = Integer.parseInt(num1)+Integer.parseInt(num2);
+        return answer;
+    }
+}
